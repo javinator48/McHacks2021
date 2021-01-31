89a61b81f4e6e9353895f09ee5d4d3b6728b0a11
EstebanMendezP <estebanmendezp777@gmail.com>
2021-01-30 22:20:20 -0400

    My methods and implementation to add and read from DataBas, look in GoogleDocs for more info

--- app/src/main/java/com/example/mchack8/LoginActivity.java
+++ app/src/main/java/com/example/mchack8/LoginActivity.java
@@ -16,6 +16,12 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class LoginActivity extends AppCompatActivity {
     private EditText email;
     private EditText password;
@@ -31,6 +37,7 @@ public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onStart() {
+
         super.onStart();
         email = findViewById(R.id.emailInput);
         password = findViewById(R.id.password);
@@ -46,6 +53,50 @@ public class LoginActivity extends AppCompatActivity {
                                 if (task.isSuccessful()){
                                     // successful
                                     Toast.makeText(getApplicationContext(), "Success!", Toast.LENGTH_LONG).show();
+
+                                    // ___________ Esteban ____________
+                                    // Write a message to the database
+                                    // Write a message to the database
+                                    String userToken;
+                                    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                                    userToken = user.getUid();
+
+                                    FirebaseDatabase database = FirebaseDatabase.getInstance();
+                                    DatabaseReference myRefMonday = database.getReference(userToken + "/Monday");
+                                    myRefMonday.child("Medicine").setValue("SomeMedicine");
+                                    DatabaseReference myRefTuesday = database.getReference(userToken + "/Tuesday");
+                                    myRefTuesday.child("Medicine").setValue("SomeMedicine");
+                                    DatabaseReference myRefWednesday = database.getReference(userToken + "/Wednesday");
+                                    myRefWednesday.child("Medicine").setValue("SomeMedicine");
+                                    DatabaseReference myRefThursday = database.getReference(userToken + "/Thursday");
+                                    myRefThursday.child("Medicine").setValue("SomeMedicine");
+                                    DatabaseReference myRefFriday = database.getReference(userToken + "/Friday");
+                                    myRefFriday.child("Medicine").setValue("SomeMedicine");
+                                    DatabaseReference myRefSaturday = database.getReference(userToken + "/Saturday");
+                                    myRefSaturday.child("Medicine").setValue("SomeMedicine");
+                                    DatabaseReference myRefSunday = database.getReference(userToken + "/Sunday");
+                                    myRefSunday.child("Medicine").setValue("SomeMedicine");
+
+                                    // Read from the database
+                                    DatabaseReference myRef = database.getReference(userToken + "/Monday/Medicine");
+                                    myRef.addValueEventListener(new ValueEventListener() {
+                                        @Override
+                                        public void onDataChange(DataSnapshot dataSnapshot) {
+                                            // This method is called once with the initial value and again
+                                            // whenever data at this location is updated.
+                                            String value = dataSnapshot.getValue(String.class);
+                                            Toast.makeText(getApplicationContext(), "Value is: " + value, Toast.LENGTH_LONG).show();
+                                        }
+
+                                        @Override
+                                        public void onCancelled(DatabaseError error) {
+                                            // Failed to read value
+                                            Toast.makeText(getApplicationContext(), "Failed to read value.", Toast.LENGTH_LONG).show();
+                                        }
+                                    });
+
+                                    // ________________________________
+
                                 } else {
                                     // failed
                                     Toast.makeText(getApplicationContext(), "Failed!", Toast.LENGTH_SHORT).show();
@@ -53,9 +104,9 @@ public class LoginActivity extends AppCompatActivity {
                             }
                         });
             }
+
         });
 
     }
-
     //edit text
 }
