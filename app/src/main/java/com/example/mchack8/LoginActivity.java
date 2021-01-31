cc26612dc5205eb13aee667128de52e051dc87f7
EstebanMendezP <estebanmendezp777@gmail.com>
2021-01-31 03:25:55 -0400

    Added section base of code for Starting Personal Databse when creats a User ; Add a medicinie to a persons DataBase ; Iterate through the medicines on a day of a persons DataBase

--- app/src/main/java/com/example/mchack8/LoginActivity.java
+++ app/src/main/java/com/example/mchack8/LoginActivity.java
@@ -59,6 +59,66 @@ public class LoginActivity extends AppCompatActivity {
                                     // successful
                                     Toast.makeText(getApplicationContext(), "Success!", Toast.LENGTH_LONG).show();
 
+                                    // ___________________________________________
+                                    // _______________ Sample Code _______________
+                                    // ___________________________________________
+                                    // All my code assume you know how to log in the person
+
+                                    // ______________________ Javin Iterator through elements of the list __________________
+                                    FirebaseDatabase database = FirebaseDatabase.getInstance();
+                                    FirebaseUser userJavin = FirebaseAuth.getInstance().getCurrentUser();
+                                    String myUserIdJavin = userJavin.getUid();
+                                    // If want to get the info on Mondays do /Monday or (/Tuesday, /Wednesday, etc)
+                                    DatabaseReference myRefList = database.getReference("Users/" + myUserIdJavin + "/Monday");
+                                    Query myTopPostsQuery = myRefList.orderByChild("starCount");
+                                    myTopPostsQuery.addValueEventListener(new ValueEventListener() {
+                                        @Override
+                                        public void onDataChange(DataSnapshot dataSnapshot) {
+                                            GenericTypeIndicator<String> t = new GenericTypeIndicator<String>() {};
+                                            for (DataSnapshot postSnapshot: dataSnapshot.getChildren()) {
+                                                        String value = postSnapshot.getValue(t);
+                                                        // YOur code goes here where String value have the current value this works like and iterator
+                                            }
+                                        }
+
+                                        @Override
+                                        public void onCancelled(DatabaseError databaseError) {
+                                            // Getting Post failed, something wrong happen
+                                        }
+                                    });
+                                    
+
+                                    // ______________________ Ethan initiate new User DataBase __________________
+                                    //Add the code below
+                                    //FirebaseDatabase database = FirebaseDatabase.getInstance();
+                                    // Delete code below if FirebaseUser already initialized
+                                    FirebaseUser userEthan = FirebaseAuth.getInstance().getCurrentUser();
+                                    String myUserIdEthan = userEthan.getUid();
+                                    DatabaseReference myRefEthan = database.getReference("Users/" + myUserIdEthan);
+                                    myRefEthan.child("Type").setValue("patient");
+
+                                    // ______________________ idk add a medicine to DAY __________________
+                                    // There will be one for each day Monday, Tuesday, ... , Sunday
+                                    //Add the code below if haven't initialize
+                                    //FirebaseDatabase database = FirebaseDatabase.getInstance();
+                                    // Delete code below if FirebaseUser already initialized
+                                    FirebaseUser userIdk = FirebaseAuth.getInstance().getCurrentUser();
+                                    String myUserIdIdk = userEthan.getUid();
+                                    // If it is Tuesday pu /Tuesday instead of /Monday
+                                    DatabaseReference myRefMonday = database.getReference("Users/" + myUserIdIdk + "/Monday");
+                                    // Put the string giving by user instead of "Intento2"
+                                    myRefMonday.push().setValue("Intento2");
+
+                                    // ___________________________________________
+                                    // ___________________________________________
+                                    // ___________________________________________
+
+
+
+
+
+
+
                                     // ___________ Esteban ____________
                                     // Write a message to the database
                                     // Write a message to the database
@@ -66,15 +126,14 @@ public class LoginActivity extends AppCompatActivity {
                                     FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
                                     myUserId = user.getUid();
 
-                                    FirebaseDatabase database = FirebaseDatabase.getInstance();
                                     /*
-                                    DatabaseReference myRefMonday = database.getReference(myUserId + "/Monday");
-                                    myRefMonday.push().setValue("Intento2");
-                                    myRefMonday.push().setValue("Intento3");
-                                    myRefMonday.push().setValue("Intento4");
-                                    myRefMonday.push().setValue("Intento5");
-                                    myRefMonday.push().setValue("Intento6");
-                                    myRefMonday.child("Medicine").setValue("SomeMedicine");
+                                    DatabaseReference myRefMonday1 = database.getReference("Users/7b39tZW1KTUvnHJWbqpdDcvgcpW2/Monday");
+                                    myRefMonday1.push().setValue("Intento2");
+                                    myRefMonday1.push().setValue("Intento3");
+                                    myRefMonday1.push().setValue("Intento4");
+                                    myRefMonday1.push().setValue("Intento5");
+                                    myRefMonday1.push().setValue("Intento6");
+                                    myRefMonday1.child("Medicine").setValue("SomeMedicine");
                                     DatabaseReference myRefTuesday = database.getReference(myUserId + "/Tuesday");
                                     myRefTuesday.child("Medicine").setValue("SomeMedicine");
                                     DatabaseReference myRefWednesday = database.getReference(myUserId + "/Wednesday");
@@ -87,7 +146,7 @@ public class LoginActivity extends AppCompatActivity {
                                     myRefSaturday.child("Medicine").setValue("SomeMedicine");
                                     DatabaseReference myRefSunday = database.getReference(myUserId + "/Sunday");
                                     myRefSunday.child("Medicine").setValue("SomeMedicine");
-                                    */
+                                    //*/
 
                                     // Read from the database
                                     DatabaseReference myRef = database.getReference(myUserId + "/Monday/Medicine");
@@ -109,10 +168,10 @@ public class LoginActivity extends AppCompatActivity {
                                     });
 
                                     // Read from a List
-                                    DatabaseReference myRefList = database.getReference(myUserId + "/Monday");
-                                    Query myTopPostsQuery = myRefList.orderByChild("starCount");
+                                    DatabaseReference myRefList2 = database.getReference(myUserId + "/Monday");
+                                    Query myTopPostsQuery2 = myRefList2.orderByChild("starCount");
                                     // My top posts by number of stars
-                                    myTopPostsQuery.addValueEventListener(new ValueEventListener() {
+                                    myTopPostsQuery2.addValueEventListener(new ValueEventListener() {
                                         @Override
                                         public void onDataChange(DataSnapshot dataSnapshot) {
                                             for (DataSnapshot postSnapshot: dataSnapshot.getChildren()) {
